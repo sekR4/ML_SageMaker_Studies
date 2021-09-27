0bd340a500cc13081c287e26bc69c9bf71c38179
sekR4 <s.kraus@eos-did.com>
2021-09-27 14:13:12 +0900

    training trials

--- Project_Plagiarism_Detection/source_sklearn/train.py
+++ Project_Plagiarism_Detection/source_sklearn/train.py
@@ -4,12 +4,13 @@ import argparse
 import os
 import pandas as pd
 
-# sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23. 
-# from sklearn.externals import joblib
-# Import joblib package directly
-import joblib
+# sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23.
+from sklearn.externals import joblib
 
-## TODO: Import any additional libraries you need to define a model
+# Import joblib package directly
+# import joblib
+# FIXME: ModuleNotFoundError: No module named 'joblib'
+from sklearn.ensemble import ExtraTreesClassifier
 
 
 # Provided model load function
@@ -18,56 +19,73 @@ def model_fn(model_dir):
     in the main if statement.
     """
     print("Loading model.")
-    
+
     # load using joblib
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     print("Done loading model.")
-    
+
     return model
 
 
 ## TODO: Complete the main code
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
+
     # All of the model parameters and training parameters are sent as arguments
     # when this script is executed, during a training job
-    
+
     # Here we set up an argument parser to easily access the parameters
     parser = argparse.ArgumentParser()
 
     # SageMaker parameters, like the directories for training data and saving models; set automatically
     # Do not need to change
-    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    
+    parser.add_argument(
+        "--output-data-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"]
+    )
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+
     ## TODO: Add any additional arguments that you will need to pass into your model
-    
+
     # args holds all passed-in arguments
     args = parser.parse_args()
 
     # Read in csv training file
     training_dir = args.data_dir
-    train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
+    train_data = pd.read_csv(
+        os.path.join(training_dir, "train.csv"), header=None, names=None
+    )
 
     # Labels are in the first column
-    train_y = train_data.iloc[:,0]
-    train_x = train_data.iloc[:,1:]
-    
-    
-    ## --- Your code here --- ##
-    
+    train_y = train_data.iloc[:, 0]
+    train_x = train_data.iloc[:, 1:]
 
-    ## TODO: Define a model 
-    model = None
-    
-    
-    ## TODO: Train the model
-    
-    
-    
+    ## --- Your code here --- ##
+
+    params = {
+        "bootstrap": False,
+        "ccp_alpha": 0.0,
+        "class_weight": None,
+        "criterion": "gini",
+        "max_depth": None,
+        "max_features": "auto",
+        "max_leaf_nodes": None,
+        "max_samples": None,
+        "min_impurity_decrease": 0.0,
+        "min_impurity_split": None,
+        "min_samples_leaf": 1,
+        "min_samples_split": 2,
+        "min_weight_fraction_leaf": 0.0,
+        "n_estimators": 100,
+        "n_jobs": -1,
+        "oob_score": False,
+        "random_state": 1,
+        "verbose": 0,
+        "warm_start": False,
+    }
+    model = ExtraTreesClassifier(**params)
+    model.fit(train_x, train_y)
+
     ## --- End of your code  --- ##
-    
 
     # Save the trained model
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
